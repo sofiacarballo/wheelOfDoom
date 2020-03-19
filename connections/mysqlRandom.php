8fd90e664122b27504f88ce80b3e950f4754da97
Sofia Carballo <sofia.carballo@live.com>
2020-03-19 22:46:44 +0100

    Added echo

--- connections/mysqlRandom.php
+++ connections/mysqlRandom.php
@@ -3,9 +3,15 @@ include("connect.php");
 
 $query="SELECT * FROM coders_list WHERE status='Alive'ORDER BY RAND() LIMIT 1";
 $result= $connect->query($query);
+
 while($row=$result->fetch_assoc()){
 ?> 
 <?php 
-echo $row['name']; 
+echo '<tr><td>' . $row['name'] . '</td></tr>'; 
 return $row['id'];
-}?>
+
+header("Location: http://localhost/phpfactoriaf5/wheelOfDoom/index.php"); die();
+
+$connect->close();
+}
+?>
