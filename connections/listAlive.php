5e85e93112ea6917fa2020f39902612d1da65e84
EllaQuímica <EllaQuimica@users.noreply.github.com>
2020-03-19 12:01:15 -0300

    Update variable name

--- connections/listAlive.php
+++ connections/listAlive.php
@@ -2,7 +2,7 @@
 include("connect.php");
 
 $query="SELECT * FROM coders_list WHERE status='Alive'";
-$result= $connection->query($query);
+$result= $connect->query($query);
 while($row=$result->fetch_assoc()){
 ?> 
 <?php echo $row['name']; }?>
