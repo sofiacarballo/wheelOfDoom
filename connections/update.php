b5b2ceb23854baf2f55fdbe2cbc163ee790b260a
EllaQuímica <EllaQuimica@users.noreply.github.com>
2020-03-19 12:04:58 -0300

    Update variable name

--- connections/update.php
+++ connections/update.php
@@ -2,7 +2,7 @@
 include ("connect.php");
 
 $sql = "UPDATE coders_list SET status='Alive' WHERE id=1";
-    if(mysqli_query($connection,$sql)){
+    if(mysqli_query($connect,$sql)){
         echo "OK Update.";
     } else {
         echo "ERROR: No Update. ";
